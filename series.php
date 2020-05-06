acd1f754f8356c2be5c767ea2792fdfcaf54fea5
JoristerWolbeek <joris.terwolbeek@outlook.com>
2020-05-06 11:28:53 +0200

    Uitwerking van de exercise

--- series.php
+++ series.php
@@ -58,8 +58,8 @@ a {
 $stmt = $pdo->prepare("SELECT titel, rating, omschrijving, seizoenen, landVanAfkomst, taal, awards, id FROM netland.inhoud WHERE id=? AND soort='series'");
 $stmt->execute([$_GET['id']]);
 while ($info = $stmt->fetch()) {
-    echo("<h1>".$info['titel']."</h1><br><b>rating </b>".$info["rating"]."</b><br><b>Land van afkomst </b>".$info["landVanAfkomst"]."<br><b>Taal </b>".$info["taal"].
-    "<br><b>Seizoenen </b>".$info["seizoenen"]."<br><br><b>Beschrijving </b><br>".$info["omschrijving"]."<br><br><b>Aantal prijzen gewonnen </b>".$info["awards"]."<br><br
+    echo("<h1>" . $info['titel'] . "</h1><br><b>rating </b>" . $info["rating"] . "</b><br><b>Land van afkomst </b>" . $info["landVanAfkomst"] . "<br><b>Taal </b>" . $info["taal"] .
+    "<br><b>Seizoenen </b>" . $info["seizoenen"] . "<br><br><b>Beschrijving </b><br>" . $info["omschrijving"] . "<br><br><b>Aantal prijzen gewonnen </b>" . $info["awards"] . "<br><br
     ><a href=http://localhost/seriesOverlord.php?id=$info[id]>Edit</a>");
 }
 
